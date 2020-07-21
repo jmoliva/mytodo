0bf70231acaffe6560625481dedcda2240c87f28
JEAN-MICHEL OLIVA <jean-michel.oliva@fr.ibm.com>
2020-07-21 11:52:25 +0200

    modif
    Signed-off-by: JEAN-MICHEL OLIVA <jean-michel.oliva@fr.ibm.com>

--- scripts/pipeline-PRE-DEPLOY.sh
+++ scripts/pipeline-PRE-DEPLOY.sh
@@ -87,8 +87,8 @@ else
   echo -e "Namespace ${CLUSTER_NAMESPACE} authorizing with private image registry using Helm chart imagePullSecret"
 fi
 
-echo "=========================================================="
-echo "CONFIGURING TILLER enabled (Helm server-side component)"
+# echo "=========================================================="
+# echo "CONFIGURING TILLER enabled (Helm server-side component)"
 
 # echo -e "\n==## Installing Helm 2.12.2"
 # wget https://storage.googleapis.com/kubernetes-helm/helm-v2.12.2-linux-amd64.tar.gz
@@ -102,7 +102,45 @@ echo "CONFIGURING TILLER enabled (Helm server-side component)"
 
 # helm init --upgrade --service-account tiller
 # kubectl rollout status -w deployment/tiller-deploy --namespace=kube-system
-helm version
+# helm version
+
+echo "=========================================================="
+echo "CHECKING HELM VERSION: matching Helm Tiller (server) if detected. "
+set +e
+LOCAL_VERSION=$( helm version --client ${HELM_TLS_OPTION} | grep SemVer: | sed "s/^.*SemVer:\"v\([0-9.]*\).*/\1/" )
+TILLER_VERSION=$( helm version --server ${HELM_TLS_OPTION} | grep SemVer: | sed "s/^.*SemVer:\"v\([0-9.]*\).*/\1/" )
+set -e
+if [ -z "${TILLER_VERSION}" ]; then
+  if [ -z "${HELM_VERSION}" ]; then
+    CLIENT_VERSION=${LOCAL_VERSION}
+  else
+    CLIENT_VERSION=${HELM_VERSION}
+  fi
+else
+  echo -e "Helm Tiller ${TILLER_VERSION} already installed in cluster. Keeping it, and aligning client."
+  CLIENT_VERSION=${TILLER_VERSION}
+fi
+if [ "${CLIENT_VERSION}" != "${LOCAL_VERSION}" ]; then
+  echo -e "Installing Helm client ${CLIENT_VERSION}"
+  WORKING_DIR=$(pwd)
+  mkdir ~/tmpbin && cd ~/tmpbin
+  curl -L https://storage.googleapis.com/kubernetes-helm/helm-v${CLIENT_VERSION}-linux-amd64.tar.gz -o helm.tar.gz && tar -xzvf helm.tar.gz
+  cd linux-amd64
+  export PATH=$(pwd):$PATH
+  cd $WORKING_DIR
+fi
+set +e
+if [ -z "${TILLER_VERSION}" ]; then
+    echo -e "Installing Helm Tiller ${CLIENT_VERSION} with cluster admin privileges (RBAC)"
+    kubectl -n kube-system create serviceaccount tiller
+    kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+    helm init --service-account tiller ${HELM_TILLER_TLS_OPTION}
+    # helm init --upgrade --force-upgrade
+    kubectl --namespace=kube-system rollout status deploy/tiller-deploy
+    # kubectl rollout status -w deployment/tiller-deploy --namespace=kube-system
+fi
+set -e
+helm version ${HELM_TLS_OPTION}
 
 echo "=========================================================="
 echo -e "CHECKING HELM releases in this namespace: ${CLUSTER_NAMESPACE}"
